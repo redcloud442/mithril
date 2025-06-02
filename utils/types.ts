e32fc81ac8273d958b476f7f6dcecb70e0e28cd2
cloudred <cloudred442@gmail.com>
2025-06-03 02:52:44 +0800

    newest commit

--- utils/types.ts
+++ utils/types.ts
@@ -194,7 +194,7 @@ export type adminUserReinvestedReportData = {
 };
 
 export type HeirarchyData = {
-  alliance_member_id: string;
+  company_member_id: string;
   user_username: string;
   user_id: string;
 };
