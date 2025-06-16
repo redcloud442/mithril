4228bdc7d83537206ccf24cdb4af546a86393533
cloudred <cloudred442@gmail.com>
2025-06-16 19:59:14 +0800

    initial commit

--- app/(protected)/contact/page.tsx
+++ app/(protected)/contact/page.tsx
@@ -1,7 +1,12 @@
-import UserContactmanage from "@/components/UserContactManage/UserContactmanage";
-
-const page = () => {
-  return <UserContactmanage />;
+const Page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-[80vh] text-center">
+      <h1 className="text-3xl font-bold text-yellow-400 mb-4">
+        🚧 Under Development
+      </h1>
+      <p className="text-lg text-orange-500">Coming Soon!</p>
+    </div>
+  );
 };
 
-export default page;
+export default Page;
